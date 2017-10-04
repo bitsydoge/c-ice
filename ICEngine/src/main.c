1b9d253d632f6871d78b52b95792c1dae124866f
Coldragon <coldragon78@gmail.com>
2017-10-04 07:50:20 +0200

    Updates for the day

--- ICEngine/src/main.c
+++ ICEngine/src/main.c
@@ -10,8 +10,8 @@ int main(int argc, char* args[])
 
 	ICE_App app = ICE_CreateApp("ICE", 480, 480);	
 
-	ICE_Texture* texture = ICE_LoadPNG(app.render, "res/img/test.png");
-	ICE_Texture* texture2 = ICE_LoadBMPAlpha(app.render, "res/img/error.bmp", ICE_Rgba(0, 200, 255, 105));
+	ICE_Texture *texture = ICE_LoadPNG(app.render, "res/img/test.png");
+	ICE_Texture *texture2 = ICE_LoadBMPAlpha(app.render, "res/img/error.bmp", ICE_Rgba(0, 200, 255, 105));
 
 	ICE_Input *input = (ICE_Input*)malloc(sizeof(ICE_Input));
 	ICE_InputReset(input);
@@ -22,7 +22,6 @@ int main(int argc, char* args[])
 	{
 		ICE_InputReturn(input);
 		ICE_RenderClear(app.render);		
-
 		ICE_TextureRender(app.render, texture, NULL, NULL);
 		ICE_TextureRender(app.render, texture2, NULL, NULL);
 		ICE_RenderPresent(app.render);
