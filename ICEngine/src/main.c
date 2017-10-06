b43169dad7dc2c57d342d459c540da2a85e3059f
Coldragon <coldragon78@gmail.com>
2017-10-06 20:23:11 +0200

    TExtureManager WOrking

--- ICEngine/src/main.c
+++ ICEngine/src/main.c
@@ -8,17 +8,23 @@ int main(int argc, char* args[])
 {
 	ICE_InitGameEngine();
 	ICE_App app = ICE_CreateApp("ICE", 500, 500);
-	ICE_Texture *texture = ICE_LoadPNG(app.render, "res/img/logo.png");
+	//ICE_Texture *texture = ICE_LoadPNG(app.render, "res/img/logo.png");
 	//ICE_Texture *texture2 = ICE_LoadBMPAlpha(app.render, "res/img/error.bmp", ICE_Rgba(0, 200, 255, 105));
-	ICE_TextureManager text = {0};
-	
-	ICE_CreateTexture(&app, text, "res/img/error.bmp", 0);
+	ICE_TextureManager text = ICE_CreateTextureManager();	
+
+
+
 	while(!app.input->quit)
 	{
+		while(1)
+		{
+			ICE_CreateTexture(&app, &text, "res/img/logo.pnggg", 0);	
+			printf("%d\n", text.nb_existing_texture);
+			SDL_Delay(50);	
+		}
 		ICE_InputReturn(app.input);
 		ICE_RenderClear(app.render);		
-		ICE_TextureRender(app.render, texture, NULL, NULL);
-	//	ICE_TextureRender(app.render, texture2, NULL, NULL);
+		ICE_TextureRender(app.render, &text.texturepack[0], NULL, NULL);
 		ICE_RenderPresent(app.render);
 	}
 
