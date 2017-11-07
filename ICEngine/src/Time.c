ebc205cc8e948bf0d7cf3e1e1e3260a22c3446c3
Coldragon <coldragon78@gmail.com>
2017-11-07 12:02:50 +0100

    Test lot of entity, added ICE_Random

--- ICEngine/src/Time.c
+++ ICEngine/src/Time.c
@@ -1,1 +1,19 @@
 ﻿#include "hdr/Time.h"
+
+int ICE_Random(int min, int max)
+{
+	int result = 0, lowest = 0, highest = 0;
+
+	if (min < max)
+	{
+		lowest = min;
+		highest = max + 1;
+	}
+	else {
+		lowest = max + 1;
+		highest = min;
+	}
+
+	result = (rand() % (highest - lowest)) + lowest;
+	return result;
+}
--- ICEngine/src/hdr/Time.h
+++ ICEngine/src/hdr/Time.h
@@ -1,6 +1,9 @@
 ﻿#ifndef DEF_ICE_TIME
 #define DEF_ICE_TIME
 
+#include <stdlib.h>
+
+int ICE_Random(int min, int max);
 
 
 #endif
