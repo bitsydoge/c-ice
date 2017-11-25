e67ea667041c0487816219877f1aec7289d8e33c
Coldragon <coldragon78@gmail.com>
2017-11-25 18:55:22 +0100

    Added cbp

--- ICEGraphics/src/hdr/Primitive.h
+++ ICEGraphics/src/hdr/Primitive.h
@@ -5,12 +5,12 @@
 #include "Render.h"
 
 /// Draw Point
-int iceDrawPoint(iceGame *game, int x, int y, const iceColor color);
+int iceDrawPoint(iceGame *game, iceVect vect, const iceColor color);
 /// Draw Line
-int iceDrawLine(iceGame *game, int x1, int y1, int x2, int y2, const iceColor color);
+int iceDrawLine(iceGame *game, iceVect vect1, iceVect vect2, const iceColor color);
 /// Draw One Pixel Line Rectangle
 int iceDrawRectangle(iceGame *game, iceBox rect, const iceColor color);
-/// Draw Filled Rectangle 
+/// Draw Filled Rectangle
 int iceDrawRectangleFill(iceGame *game, iceBox rect, const iceColor color);
 
 // gl like
