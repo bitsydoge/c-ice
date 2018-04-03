55c4eb45a0e5f26f5bcc0e8babde73b039927bd9
Coldragon <coldragon78@gmail.com>
2018-04-03 20:13:01 +0200

    doc

--- doc/html/search/all_15.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['w',['w',['../struct_i_c_e___window.html#a262e2d6fe0b209be4276993e4f36b295',1,'ICE_Window::w()'],['../struct_i_c_e___texture.html#a2b470e57e2d1e52fa1f9aeaab232fd20',1,'ICE_Texture::w()'],['../struct_i_c_e___box.html#a8fbcaf12b2f3cf8df57f280e5392e5f3',1,'ICE_Box::w()']]],
-  ['wheeldown',['wheeldown',['../struct_i_c_e___input.html#a58fbd7af7b3e6cfed205be482997b3cc',1,'ICE_Input']]],
-  ['wheelup',['wheelup',['../struct_i_c_e___input.html#a51f909f76473fdc86c93712b9e51299e',1,'ICE_Input']]],
-  ['window',['window',['../struct_i_c_e___game.html#aa34cbbb701f9f7d73ce897a1e4fc8d2e',1,'ICE_Game']]],
-  ['window_2ec',['Window.c',['../_window_8c.html',1,'']]],
-  ['window_2eh',['Window.h',['../_window_8h.html',1,'']]]
-];
