c34caee9d3ff204c1920fa6df5d465655306dfcc
Coldragon <coldragon78@gmail.com>
2018-04-28 21:15:46 +0200

    Switched project test

---
+++ TestProject/2DWatch/src/weapon.c
@@ -0,0 +1,17 @@
+﻿#include "weapon.h"
+
+Game_Weapon Game_Weapon_Init(wchar_t* name, ICE_Float damage, ICE_Float speed, ICE_Float weight)
+{
+	Game_Weapon weapon = {
+		ICE_String_Init(name),
+		damage,
+		speed,
+		weight };
+
+	return weapon;
+}
+
+void Game_Weapon_Destroy(Game_Weapon *weapon)
+{
+	ICE_String_Delete(weapon->name);
+}
