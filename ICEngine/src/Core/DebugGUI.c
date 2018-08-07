51c688fb7cd24fc9f6595dc2d27e3f7a25fb608d
Coldragon <coldragon78@gmail.com>
2018-08-07 23:18:47 +0200

    readme update

--- ICEngine/src/Core/DebugGUI.c
+++ ICEngine/src/Core/DebugGUI.c
@@ -13,35 +13,9 @@ void ICE_DebugGUI_ThreadStart()
 	game.guiDebugThread = SDL_CreateThread(ICE_DebugGUI_ThreadFunction, "DebugGUI_IUP", NULL);
 }
 
-int btn_exit_cb(Ihandle *self)
-{
-	/* Exits the main loop */
-	return IUP_CLOSE;
-}
-
 void ICE_DebugGUI_ThreadFunction()
 {
-	Ihandle *dlg, *button, *label, *vbox;
-
-	label = IupLabel("Hello world from IUP.");
-	button = IupButton("OK", NULL);
-	vbox = IupVbox(
-		label,
-		button,
-		NULL);
-	IupSetAttribute(vbox, "ALIGNMENT", "ACENTER");
-	IupSetAttribute(vbox, "GAP", "10");
-	IupSetAttribute(vbox, "MARGIN", "10x10");
-
-	dlg = IupDialog(vbox);
-	IupSetAttribute(dlg, "TITLE", "Hello World 5");
-
-	/* Registers callbacks */
-	IupSetCallback(button, "ACTION", (Icallback)btn_exit_cb);
-
-	IupShowXY(dlg, IUP_CENTER, IUP_CENTER);
-
-	IupMainLoop();
+	IupMessageError(NULL, "Hello !!");
 }
 
 void ICE_DebugGUI_Close()
--- README.md
+++ README.md
@@ -40,14 +40,23 @@ _A simple Game Engine with basic Camera, Entity, Texture, Audio, Map ..._
 
 ICE_Game_Create("Hello World", 800, 480)
 {
-	ICE_Font_Load("font_path.ttf");
+	ICE_Font_Load("font.ttf");
 	ICE_Index man = ICE_LabelManager_Insert(NULL);
-	ICE_Index nbr = ICE_Label_Insert(NULL, man, L"Hello World", ICE_Vect_Null, 50, ICE_LabelType_World);
+	ICE_Index nbr = ICE_Label_Insert
+	(
+		NULL, 
+		man, 
+		"Hello World", 
+		ICE_Vect_Null, 
+		50, 
+		ICE_LabelType_World
+	);
 }
 
 ICE_Game_Update()
 {
-	if (ICE_Input_Key(ICE_KEY_ESCAPE)) ICE_Input_Quit();
+	if (ICE_Input_Key(ICE_KEY_ESCAPE)) 
+		ICE_Input_Quit();
 }
 
 ICE_Game_Destroy() {}
