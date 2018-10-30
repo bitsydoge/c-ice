33eb07bef0a05cb657e0d3552a24569906070925
Coldragon <coldragon78@gmail.com>
2018-10-30 13:35:23 +0100

    added typedef for ICE_ID type  (ICE_EntityID, ICE_TextureID etc ...)

--- ICEngine/src/Core/TypesCore.h
+++ ICEngine/src/Core/TypesCore.h
@@ -13,6 +13,21 @@
 // Typedef
 // --------------------------------------
 
+#ifndef ICE_INDEX_CORETYPE_DEFINED
+#define ICE_INDEX_CORETYPE_DEFINED
+
+typedef ICE_ID ICE_EntityID;
+typedef ICE_ID ICE_LabelID;
+typedef ICE_ID ICE_GuiID;
+
+typedef ICE_ID ICE_SpriteID;
+typedef ICE_ID ICE_TextureID;
+typedef ICE_ID ICE_FontID;
+typedef ICE_ID ICE_SoundID;
+typedef ICE_ID ICE_MusicID;
+
+#endif
+
 #ifndef ICE_BOOL_DEFINED
 #define ICE_BOOL_DEFINED
 /**
