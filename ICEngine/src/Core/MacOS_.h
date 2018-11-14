59d479a22a13087be1aa74d202dc5db17512b8de
Coldragon <coldragon78@gmail.com>
2018-11-14 13:47:17 +0100

    Little edit in macos_.h et .c

--- ICEngine/src/Core/MacOS_.h
+++ ICEngine/src/Core/MacOS_.h
@@ -11,11 +11,11 @@
 #if defined(__APPLE__)
 #include <unistd.h>
 #include <CoreFoundation/CoreFoundation.h> //
-ICE_Char * ICE_MacOS_GetResourcesDirectory();
+ICE_StringStd ICE_MacOS_GetResourcesDirectory();
 ICE_Bool ICE_MacOS_SetWorkingDirectory(ICE_StringStd path);
 #else
-#define ICE_MacOS_GetResourcesDirectory()
-#define ICE_MacOS_SetWorkingDirectory(...)
+#define ICE_MacOS_GetResourcesDirectory() (ICE_StringStd) "NOT MACOS"
+#define ICE_MacOS_SetWorkingDirectory(...) ICE_False
 #endif
 
 #endif //ICENGINE_MACOS_H
