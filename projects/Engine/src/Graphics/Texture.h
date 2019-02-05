120aa695cf834df9e3ade866817e4dd934bee624
coldragon <coldragon78@gmail.com>
2019-02-05 18:50:46 +0100

    Added same loading layout for music (as texture)

--- projects/Engine/src/Graphics/Texture.h
+++ projects/Engine/src/Graphics/Texture.h
@@ -10,8 +10,11 @@
 void ICE_TextureManager_Init();
 void ICE_TextureManager_Destroy();
 
-#define ICE_Texture_LoadFromEmbedded(S) ICE_Texture_Load_RW(SDL_RWFromConstMem(S, S##_length));\
-	ICE_Log_Succes(ICE_MACROTOOLS_STRINGIZE(Texture number %ld loaded from embedded binary : ## S), ICE_Texture_GetLastLoaded());\
+#define ICE_Texture_Load_MEM(S) ICE_Texture_Load_RW(SDL_RWFromConstMem(S, S##_length));\
+	if(ICE_Texture_GetLastLoaded() != (ICE_ID)-1)\
+		ICE_Log_Succes(ICE_MACROTOOLS_STRINGIZE(Texture loaded from memory : ID(%ld) Token(##S##)), ICE_Texture_GetLastLoaded());\
+	else\
+		ICE_Log_printf(ICE_MACROTOOLS_STRINGIZE(Texture didn t loaded from token : ##S));\
 
 ICE_TextureID ICE_Texture_GetLastLoaded();
 ICE_ID ICE_Texture_Load(ICE_StringStd path_);
@@ -19,6 +22,8 @@ ICE_Texture ICE_Texture_Build_RW(SDL_RWops * rwops);
 ICE_ID ICE_Texture_Load_RW(SDL_RWops * rwops_);
 void ICE_Texture_Destroy(ICE_ID texture_);
 void ICE_Texture_Free(ICE_Texture * texture_);
+
+// Texture Tools
 unsigned int ICE_Texture_GetW(ICE_ID texture_);
 unsigned int ICE_Texture_GetH(ICE_ID texture_);
 ICE_Texture * ICE_Texture_Get(ICE_ID texture_);
